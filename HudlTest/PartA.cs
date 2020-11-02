fd4bb027cffffb7624fd6d1896591b76a6e456b7
Simone Taddei <tadde1983@libero.it>
2020-11-02 23:52:05 +0000

    first commit

---
+++ HudlTest/PartA.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HudlTest
+{
+    [TestClass]
+    public class PartA
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ HudlTest/PartB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HudlTest
+{
+    [TestClass]
+    public class PartB
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
